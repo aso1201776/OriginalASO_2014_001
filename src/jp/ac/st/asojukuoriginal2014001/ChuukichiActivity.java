273863ba23e003aabc79e4d536a45f6ef2f7c489
student <student@PC111S10.fgakusei.local>
2014-05-28 15:22:51 +0900

    おみくじ

---
+++ src/jp/ac/st/asojukuoriginal2014001/ChuukichiActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.st.asojukuoriginal2014001;
+
+import android.os.Bundle;
+
+public class ChuukichiActivity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cyuukichi_activity);
+	}
+
+}
---
+++ src/jp/ac/st/asojukuoriginal2014001/DaikichiActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.st.asojukuoriginal2014001;
+
+import android.os.Bundle;
+
+public class DaikichiActivity extends MainActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.daikichi_activity);
+	}
+
+}
